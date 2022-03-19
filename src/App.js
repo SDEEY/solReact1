1a525dd15f7f2ce4baf502939f1815e0b63381ea
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-03-19 22:03:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.25
-const image = 'https://pbs.twimg.com/profile_images/1489281560780156932/THoCZ1AQ_400x400.jpg'
-const Title = 'Skater Monkey Gang'
+const solAmount = 0.1
+const image = 'https://pbs.twimg.com/media/FOOODa0XsAIqYRi?format=jpg&name=medium'
+const Title = 'Solana Secret Society'
 
 document.title = Title
 
