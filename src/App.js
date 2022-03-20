561f7963bcc36e54b80204313591bffefc95ccd8
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-03-20 18:19:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.1
-const image = 'https://pbs.twimg.com/media/FOOODa0XsAIqYRi?format=jpg&name=medium'
-const Title = 'Solana Secret Society'
+const solAmount = 0.2
+const image = 'https://pbs.twimg.com/profile_images/1477361290339393545/QGa5EVd0_400x400.jpg'
+const Title = 'MetaMoonMission'
 
 document.title = Title
 
