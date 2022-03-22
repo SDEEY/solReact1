64ffbe533f20e09957fc35ea2d793c074a9e35c6
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-03-22 20:58:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = 0.02
-const image = 'https://pbs.twimg.com/profile_images/1505299365170393088/OcfY0CYt_400x400.jpg'
+const image = 'https://pbs.twimg.com/profile_images/1505299365170393088/OcfY0CYt_400x400.jpg' 
 const Title = 'Puff Penguins Club'
 
 document.title = Title
