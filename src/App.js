ecd7c3587ea5bef7efd84a9defcb857a3474ff80
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-03-28 19:25:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.05
-const image = 'https://pbs.twimg.com/profile_images/1505541954507001858/EXwD1num_400x400.jpg' 
-const Title = 'Solana Kyber Ape'
+const solAmount = 0.4
+const image = 'https://pbs.twimg.com/profile_images/1501950539143077899/TcrETrb7_400x400.jpg' 
+const Title = 'Sol Dog Club'
 
 document.title = Title
 
