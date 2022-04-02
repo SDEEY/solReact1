101d7bd4c48960efaeaeb6b75f09d5c852dfcabd
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-04-02 20:07:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = 0.22
-const image = 'https://pbs.twimg.com/profile_images/1503383685160124425/Tk77ocSH_400x400.jpg' 
-const Title = 'ShmallowzNFT'
+const image = 'https://pbs.twimg.com/profile_images/1505441244511879170/SRdykjfB_400x400.jpg' 
+const Title = 'Bored Gorilla'
 
 document.title = Title
 
