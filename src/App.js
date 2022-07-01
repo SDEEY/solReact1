47ff36d31f98f791236f7d59fbad0e55eaa16191
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-01 21:50:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 1
-const image = 'https://howrare.is/drop_logos/5414_WSWuuez4.jpg' 
-const Title = 'Metafigz Body'
-const supply = 555
+const solAmount = 0.09
+const image = 'https://howrare.is/drop_logos/5451_VVweeaKq.jpg' 
+const Title = 'Clout Dao'
+const supply = 3333
 
 document.title = Title
 
