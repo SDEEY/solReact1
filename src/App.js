8f3d89ee41b9db8b6fc27df2572e8e9045a2c526
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-02 01:50:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = 0
-const image = 'https://howrare.is/drop_logos/5461_69nWxYzL_400x400.jpg' 
-const Title = 'The Frameboy Inner Trip'
-const supply = 1111
+const image = 'https://howrare.is/drop_logos/5323_lN6SLDpT.jpg' 
+const Title = 'MAD ZOMBIE'
+const supply = 666
 
 document.title = Title
 
