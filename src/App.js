c23bb46f26f9fa989d59ed2732af1e67ec478e47
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-04 00:48:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.33
-const image = 'https://media.discordapp.net/attachments/971789875043000420/987362596191629333/21.png?width=475&height=475' 
-const Title = 'Hunted Bears'
-const supply = 3333
+const solAmount = 0.7
+const image = 'https://howrare.is/drop_logos/5395_3fLR1oMT_400x400.jpg' 
+const Title = 'Sol Ape Girls'
+const supply = 653
 
 document.title = Title
 
