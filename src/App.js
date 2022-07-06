5a7cf3248e3ae2fe4e401d689890339ea6a4812f
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-06 21:57:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0
-const image = 'https://howrare.is/drop_logos/5495_g5H6B_F7.jpg' 
-const Title = 'SOLANA ELEMENTS'
-const supply = 1444
+const solAmount = 3
+const image = 'https://howrare.is/drop_logos/4970_Y-fYo8tJ.jpg' 
+const Title = 'Asset Bender NFT'
+const supply = 6969
 
 document.title = Title
 
