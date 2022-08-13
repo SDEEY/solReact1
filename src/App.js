c22de758196b1510fd4b6380b4e62f8bbb551b0a
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-13 21:53:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.1
-const image = 'https://howrare.is/drop_logos/5738_4d9fGpI2.png' 
-const Title = 'SPACEMAN ON SOLANA'
-const supply = 1111
+const solAmount = 0
+const image = 'https://nftsolana.io/wp-content/uploads/2022/08/fye4ZadP_400x400.jpeg' 
+const Title = 'Clout Bots'
+const supply = 5000
 
 document.title = Title
 
