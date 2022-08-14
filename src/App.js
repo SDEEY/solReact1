249a8d7912353ddea359f22b1ad3480303f896d8
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-14 19:48:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.15
-const image = 'https://pbs.twimg.com/profile_images/1532911335083761666/-n6eeWWV_400x400.jpg' 
-const Title = 'JUNGLE JUICE NFT'
-const supply = 5555
+const solAmount = 0.4
+const image = 'https://howrare.is/drop_logos/5755_mKH5n0F5.jpg' 
+const Title = 'Meta Banana'
+const supply = 222
 
 document.title = Title
 
